77e7b2e19481a55891184fc3e8fd69d436e6639c
Mr. Fluffy <git@sixearsm>
2023-12-20 08:35:18 +0000

    _

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, hixpkgs, hpkgs1, myPkgs }:
+  outputs = { self, nixpkgs, flake-utils, hpkgs1, myPkgs }:
     flake-utils.lib.eachSystem ["x86_64-linux"] (system:
       let
         pkgs    = nixpkgs.legacyPackages.${system};
